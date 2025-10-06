f924c8b84efe91772741daa45f470242af7eb7dd
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-10-06 13:28:09 +0600

    Update SecurityConfig.java

--- insurancemangement/src/main/java/com/sadiar/insurancemangement/security/SecurityConfig.java
+++ insurancemangement/src/main/java/com/sadiar/insurancemangement/security/SecurityConfig.java
@@ -107,7 +107,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://192.168.88.250:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:5000/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
